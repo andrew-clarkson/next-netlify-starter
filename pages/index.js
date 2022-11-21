2ff17642dda3554104f2101edc956b99e7b549a5
Andrew Clarkson <94328514+andrew-clarkson@users.noreply.github.com>
2022-11-21 11:25:12 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,17 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+    
+        <div>
+          <script
+            src="https://atc-w-gabesfix.unii.rs/embed2.js"
+            data-widget-type="universe-calendar"
+            data-target-type="time-slots"
+            data-target-id="637b9ee837beef002f31ad2f"
+            data-filter="true"
+            data-state="buttonColor=#3A66E5&buttonText=Get Tickets" >
+          </script>
+        </div>
       </main>
 
       <Footer />
